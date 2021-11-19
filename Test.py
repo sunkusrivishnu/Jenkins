8fcdbcf5e476f1ca35cfc7b473e1f8e0ae7fac4c
sunkusrivishnu <54676556+sunkusrivishnu@users.noreply.github.com>
2021-11-19 12:00:08 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -11,16 +11,6 @@ class TestSum(unittest.TestCase):
         """
         data = [0, 1, 2, 3, 4, 5]
         result1 = factorial(data[0])
-        result2 = factorial(data[1])
-	result3 = factorial(data[2])
-	result4 = factorial(data[3])
-	result5 = factorial(data[4])
-	result6 = factorial(data[5])
         self.assertEqual(result1, 1)
-        self.assertEqual(result2, 1)
-	self.assertEqual(result3, 2)
-	self.assertEqual(result4, 6)
-	self.assertEqual(result5, 24)
-	self.assertEqual(result6, 120)
 if __name__ == '__main__':
     unittest.main()
